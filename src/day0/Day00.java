4c2b3875e85f75dfe8a5caaceec70130b388da2f
arman2275 <mpiyas782@gmail.com>
2023-08-30 12:27:02 -0400

    2nd day hw

--- src/day0/Day00.java
+++ src/day0/Day00.java
@@ -4,5 +4,20 @@ public class Day00 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 	}
+	
+	/*
+	 * 
+	 * git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/arman2275/gitTestDemo.git
+git push -u origin main
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
 
 }
---
+++ src/day1/Demo2ndHw.java
@@ -0,0 +1,6 @@
+package day1;
+
+public class Demo2ndHw {
+	//you did a lot
+
+}
